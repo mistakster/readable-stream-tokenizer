e196d38dc9ea05b93c9d8743b001930605c1956c
Vladimir Kuznetsov <mistakster@gmail.com>
2021-05-02 12:15:52 +0500

    added support for the closed streams

--- test/createMockReadableStream.mjs
+++ test/createMockReadableStream.mjs
@@ -1,16 +1,26 @@
+function delay(timeout) {
+  return new Promise(resolve => setTimeout(resolve, timeout));
+}
+
 /**
  * @param {function: Generator<Uint8Array>} generator
+ * @param {Promise} closed
  * @returns {ReadableStream}
  */
-export function createMockReadableStream(generator) {
+export function createMockReadableStream(generator, closed) {
   const gen = generator();
 
   return {
     getReader() {
       return {
         async read() {
-          return gen.next();
-        }
+          const result = await gen.next();
+
+          await delay(100);
+
+          return result;
+        },
+        closed
       }
     }
   };
--- test/tokenizer.spec.mjs
+++ test/tokenizer.spec.mjs
@@ -2,6 +2,8 @@ import assert from 'assert';
 import { Tokenizer } from '../lib/tokenizer.mjs';
 import { createMockReadableStream } from './createMockReadableStream.mjs';
 
+const unfinishedPromise = new Promise(() => {});
+
 describe('Tokenizer', () => {
   it('should read Uint32', async () => {
     function* generator() {
@@ -10,7 +12,7 @@ describe('Tokenizer', () => {
       yield new Uint8Array([3, 4]);
     }
 
-    const tokenizer = new Tokenizer(createMockReadableStream(generator));
+    const tokenizer = new Tokenizer(createMockReadableStream(generator, unfinishedPromise));
 
     assert.strictEqual(await tokenizer.readUint32(), 528408);
     assert.strictEqual(await tokenizer.readUint32(), 539504696);
@@ -24,7 +26,7 @@ describe('Tokenizer', () => {
       yield new Uint8Array([3, 4]);
     }
 
-    const tokenizer = new Tokenizer(createMockReadableStream(generator));
+    const tokenizer = new Tokenizer(createMockReadableStream(generator, unfinishedPromise));
 
     assert.deepStrictEqual(await tokenizer.readUint8Array(2), new Uint8Array([0, 8]));
     assert.deepStrictEqual(await tokenizer.readUint8Array(6), new Uint8Array([16, 24, 32, 40, 48, 56]));
@@ -35,13 +37,37 @@ describe('Tokenizer', () => {
       yield new Uint8Array([1, 2, 3]);
     }
 
-    const tokenizer = new Tokenizer(createMockReadableStream(generator));
+    const tokenizer = new Tokenizer(createMockReadableStream(generator, unfinishedPromise));
 
     await assert.rejects(async () => {
       await tokenizer.readUint32();
     }, (err) => {
       assert.strictEqual(err.name, 'Error');
       assert.strictEqual(err.message, 'ReadableStream has been finished');
+      return true;
+    });
+  });
+
+  it('should throw an error on reading from the closed stream', async () => {
+    let reject;
+    const closed = new Promise((_resolve, _reject) => reject = _reject);
+
+    function* generator() {
+      yield new Uint8Array([1, 2, 3, 4]);
+
+      reject(new Error('Abort'));
+
+      yield new Uint8Array([1, 2, 3, 4]);
+    }
+
+    const tokenizer = new Tokenizer(createMockReadableStream(generator, closed));
+
+    await assert.rejects(async () => {
+      await tokenizer.readUint32();
+      await tokenizer.readUint32();
+    }, (err) => {
+      assert.strictEqual(err.name, 'Error');
+      assert.strictEqual(err.message, 'Abort');
       return true;
     });
   });
