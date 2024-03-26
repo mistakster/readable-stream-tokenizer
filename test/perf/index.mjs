e20854a49b892953372ae4b3251cf5a17799d403
Vladimir Kuznetsov <mistakster@gmail.com>
2024-03-26 16:05:37 +0400

    added performance improvements

---
+++ test/perf/index.mjs
@@ -0,0 +1,23 @@
+import { performance, createHistogram } from "node:perf_hooks";
+import { Tokenizer } from "../../lib/Tokenizer.mjs";
+
+function* generator() {
+  while (true) {
+    const length = Math.floor(10 + 290 * Math.random());
+    yield new Uint8Array(length);
+  }
+}
+
+const tokenizer = new Tokenizer(ReadableStream.from(generator()));
+
+const histogram = createHistogram();
+
+const readUint8Array = performance.timerify(tokenizer.readUint8Array.bind(tokenizer), {
+  histogram
+});
+
+for (let i = 0; i < 1e7; i++) {
+  await readUint8Array(100);
+}
+
+console.log(histogram);
