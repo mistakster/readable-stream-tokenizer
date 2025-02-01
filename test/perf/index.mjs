cbc426f8c477c51ef521258d360b25ffe6516af7
Vladimir Kuznetsov <mistakster@gmail.com>
2025-02-01 19:57:22 +0400

    changed performance test

--- test/perf/index.mjs
+++ test/perf/index.mjs
@@ -1,20 +1,10 @@
 import { performance, createHistogram } from "node:perf_hooks";
-import { getRandomValues } from "node:crypto";
-import { Tokenizer } from "../../lib/TokenizerFast.mjs";
+import { Tokenizer } from "../../lib/Tokenizer.mjs";
 
 function* generator() {
-  const random = new Uint8Array(10000);
-  let i = 0;
-
   while (true) {
-    if (i === 0) {
-      getRandomValues(random);
-    }
-
-    const length = 10 + random[i++];
+    const length = 10 + Math.floor(Math.random() * 1000);
     yield new Uint8Array(length);
-
-    i = (i + 1) % random.length;
   }
 }
 
@@ -28,21 +18,6 @@ const method = performance.timerify(tokenizer.readUint8Array.bind(tokenizer), {
 
 for (let i = 0; i < 1e6; i++) {
   await method(1000);
-  // if (i % 100000000 === 0) {
-  //   await new Promise(resolve => setTimeout(resolve, 100));
-  // }
 }
 
 console.log(histogram);
-
-try {
-  if (global.gc) {
-    global.gc();
-  } else {
-    console.log('GC is not available');
-  }
-} catch (e) {
-  console.log("`node --expose-gc index.js`");
-}
-
-await new Promise(resolve => setTimeout(resolve, 10000));
